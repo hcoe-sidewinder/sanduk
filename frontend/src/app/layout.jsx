cd434295b0190c896f3e2fe9cfdefa9bd5971a83
amritchapai <chapaiamrit@gmail.com>
2025-06-13 13:30:20 +0545

    login started

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -19,8 +19,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased w-screen`}
       >
         {children}
       </body>
