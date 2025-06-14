8ac8b8ac3ef0a61f0d63b246758f24507f2cad22
FosRex <gahireanshu1011@gmail.com>
2025-06-15 01:10:54 +0545

    Not tested implementation of shareProfile

--- backend/resources/users/userId/share.ts
+++ backend/resources/users/userId/share.ts
@@ -6,6 +6,8 @@ import { Message } from "../../../common/messages";
 import { ProfileShare } from "../../../models/profile-share";
 import { HTTPStatus } from "../../../common/http-status";
 import { sendFCMNotification } from "../../lib/notification-helper";
+import { FCMToken } from "../../../models/fcm-token";
+import { HTTP404Error } from "../../../common/errors";
 
 const validators: Validators = checkSchema({
   userId: {
@@ -52,6 +54,15 @@ export const shareProfile: Resource = post(
     }
 
     await ProfileShare.create({ patient, doctor: doctorId });
+
+    const docFCM = await FCMToken.findOne({ userId: doctorId });
+
+    if (!docFCM) throw new HTTP404Error(Message.USER_NOT_FOUND);
+
+    sendFCMNotification({
+      token: docFCM.fcmToken!!,
+      notification: { title: "A user shared a profile with you", body: "Test" },
+    });
 
     res.empty(Message.CREATED, HTTPStatus.CREATED);
   },
