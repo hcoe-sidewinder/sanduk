d829cba386a0dff0894fb48aa4107f76ddd2c01c
FosRex <gahireanshu1011@gmail.com>
2025-06-14 15:55:22 +0545

    Added endpoints for vaccines

--- backend/resources/register-routes.ts
+++ backend/resources/register-routes.ts
@@ -8,10 +8,13 @@ import { newUserSession } from "./users/new-session";
 import { validateToken, verifyToken } from "../middlewares/authentication/middleware";
 import { newReports } from "./users/userId/new-report";
 import { getReports } from "./users/userId/get-reports";
+import { newVaccine } from "./users/userId/new-vaccine";
+import { getVaccines } from "./users/userId/get-vaccines";
 
 export const resources: Record<string, Record<string, Resource>> = {
   users: { newUser, getUsers, newUserSession },
   labReports: { newReports, getReports },
+  vaccines: { newVaccine, getVaccines },
 };
 
 export const registerResources = (router: Router) => {
---
+++ backend/resources/users/userId/get-vaccines.ts
@@ -0,0 +1,54 @@
+import type { Request, Response } from "express";
+import { get, type Resource, type Validators } from "../../lib/resources";
+import { checkSchema, matchedData } from "express-validator";
+import { Message } from "../../../common/messages";
+import mongoose from "mongoose";
+import { Vaccine, type IVaccine } from "../../../models/vaccine";
+
+const validators: Validators = checkSchema({
+  userId: {
+    in: "params",
+    custom: {
+      options: (value: string) => {
+        if (value === "@me") {
+          return true;
+        }
+        return mongoose.Types.ObjectId.isValid(value);
+      },
+      errorMessage: Message.NOT_A_MONGOID_OR_ME,
+    },
+  },
+  name: {
+    in: "query",
+    trim: true,
+    optional: true,
+  },
+});
+
+export const getVaccines: Resource = get(
+  "/users/:userId/vaccines",
+  {
+    auth: true,
+    validators,
+  },
+  async (req: Request, res: Response) => {
+    const { userId, name } = matchedData<{ userId: mongoose.Types.ObjectId | "@me"; name: string }>(req);
+
+    let patient: mongoose.Types.ObjectId;
+    if (userId === "@me") {
+      patient = new mongoose.Types.ObjectId(req.userContext.sub);
+    } else {
+      patient = new mongoose.Types.ObjectId(userId);
+    }
+
+    const query: mongoose.FilterQuery<IVaccine> = { patient };
+
+    if (name) {
+      query["name"] = name;
+    }
+
+    const vaccines = await Vaccine.find(query).lean();
+
+    res.success(vaccines);
+  },
+);
