43737094d4bb97218261b34df935acac6decc54d
FosRex <gahireanshu1011@gmail.com>
2025-06-14 22:15:46 +0545

    Add firebase app

---
+++ backend/common/firebase.ts
@@ -0,0 +1,12 @@
+import data from "../firebase-config.json";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = data;
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
